cbe6b972b592ecec8b4aa218763e71d75d5f5e28
Joshua <joshua6300018@gmail.com>
2018-05-06 03:27:46 +0800

    daily update

--- autoPush.sh
+++ autoPush.sh
@@ -2,7 +2,7 @@
 
 git add .
 git commit -m "daily update"
-git push
+git push origin master
 #/usr/bin/git fetch --depth=1
 #/usr/bin/git reflog expire --expire-unreachable=now --all
 #/usr/bin/git gc --aggressive --prune=all
